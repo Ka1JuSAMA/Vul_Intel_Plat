4d0bc2a79c050a1a699e90c29a1bf6ec8f906a0b
Zhengjie Jiang <jzjjiangzj@gmail.com>
2023-10-13 21:04:14 +1030

    admin logout
    added Logout for admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
@@ -10,4 +11,15 @@ class AdminController extends Controller
 
         return view('admin.index');
     }
+
+    public function AdminLogout(Request $request){
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    }
 }
+
--- routes/web.php
+++ routes/web.php
@@ -35,4 +35,5 @@ Route::middleware(['auth','role:admin'])->group(function (){
 
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
 
+    Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
 });
