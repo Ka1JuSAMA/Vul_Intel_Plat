0d465a7d65dac47fc3f2e286be8d4bde38c6b13d
loveikuto <hyw0305021@126.com>
2023-11-19 14:08:29 +1030

    Add News Feed on dashboard

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/News_Feed', function () {
+    return view('/admin/news_feed');
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
